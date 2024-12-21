a76c65699dffe9c65979371f86a92114ae0bcfd4
KushanTharaka <kushan.tharaka@axiatadigitallabs.com>
2024-12-21 16:12:37 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,66 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/single",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Single.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Single = () => {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+export default Single
+
